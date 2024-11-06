960bbcdac506401ac6d2aa5ff985bc225dfaf2b7
Diego Freeman <diegofreeman78@gmail.com>
2024-11-06 10:21:29 +0100

    Initial commit

---
+++ app/(unprotected)/_layout.tsx
@@ -0,0 +1,29 @@
+import { Stack, useRouter } from 'expo-router';
+import React from 'react';
+import { Pressable, Text } from 'react-native';
+
+const UnprotectedLayout = () => {
+  const router = useRouter();
+
+  const onPress = () => {
+    router.back();
+  };
+  return (
+    <Stack screenOptions={{ headerShadowVisible: false }}>
+      <Stack.Screen
+        name="login"
+        options={{
+          title: 'Amazon.sg',
+          headerTitleAlign: 'center',
+          headerLeft: () => (
+            <Pressable onPress={onPress} style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}>
+              <Text>Cancel</Text>
+            </Pressable>
+          ),
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default UnprotectedLayout;
---
+++ app/(unprotected)/login.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+
+import { Wrapper } from '~/components/ui/Wrapper';
+
+export default function login() {
+  return (
+    <Wrapper>
+      <Text>login</Text>
+    </Wrapper>
+  );
+}
