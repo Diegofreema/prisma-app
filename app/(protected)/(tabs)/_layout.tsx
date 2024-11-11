539c4a98428f5e511d804e324054f56bf9519764
Diego Freeman <diegofreeman78@gmail.com>
2024-11-11 13:07:15 +0100

    finished header

--- app/(protected)/(tabs)/_layout.tsx
+++ app/(protected)/(tabs)/_layout.tsx
@@ -1,45 +1,64 @@
 import { Tabs } from 'expo-router';
+import { StatusBar } from 'react-native';
+import { CartIcon } from '~/components/CartIcon';
 
 import { TabsIcon } from '~/components/TabsIcon';
 import { colors } from '~/constants';
 
 export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarInactiveTintColor: colors.dark,
-        tabBarActiveTintColor: colors.yellow,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ focused, size }) => <TabsIcon focused={focused} name="home" size={size} />,
-        }}
-      />
-      <Tabs.Screen
-        name="menu"
-        options={{
-          title: 'Menu',
-          tabBarIcon: ({ focused, size }) => <TabsIcon focused={focused} name="bars" size={size} />,
-        }}
-      />
-      <Tabs.Screen
-        name="more"
-        options={{
-          title: 'More',
-          tabBarIcon: ({ focused, size }) => (
-            <TabsIcon focused={focused} name="amazon" size={size} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="account"
-        options={{
-          title: 'Account',
-          tabBarIcon: ({ focused, size }) => <TabsIcon focused={focused} name="user" size={size} />,
-        }}
-      />
-    </Tabs>
+    // @ts-ignore
+    <>
+      <StatusBar barStyle="light-content" />
+      <Tabs
+        screenOptions={{
+          tabBarInactiveTintColor: colors.dark,
+          tabBarActiveTintColor: colors.yellow,
+          headerTintColor: colors.yellow,
+          headerStyle: { backgroundColor: colors.dark },
+          headerRight: () => <CartIcon />,
+        }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: '',
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ focused, size }) => (
+              <TabsIcon focused={focused} name="home" size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="menu"
+          options={{
+            title: '',
+            tabBarLabel: 'Menu',
+            tabBarIcon: ({ focused, size }) => (
+              <TabsIcon focused={focused} name="bars" size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="more"
+          options={{
+            title: '',
+            tabBarLabel: 'More',
+            tabBarIcon: ({ focused, size }) => (
+              <TabsIcon focused={focused} name="amazon" size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="account"
+          options={{
+            title: '',
+            tabBarLabel: 'Account',
+            tabBarIcon: ({ focused, size }) => (
+              <TabsIcon focused={focused} name="user" size={size} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
---
+++ components/CartIcon.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable prettier/prettier */
+
+import { AntDesign } from '@expo/vector-icons';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+import { colors } from '~/constants';
+
+export const CartIcon = () => {
+  const onPress = () => {};
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [styles.press, { opacity: pressed ? 0.5 : 1 }]}>
+      <AntDesign name="shoppingcart" color="white" size={23} />
+      <View style={styles.abs}>
+        <Text style={{ color: 'white', fontSize: 10 }}>0</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  press: {
+    backgroundColor: colors.gray,
+    height: 40,
+    width: 40,
+    padding: 5,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 10,
+  },
+
+  abs: {
+    position: 'absolute',
+    top: -5,
+    right: 0,
+    backgroundColor: colors.yellow,
+    height: 15,
+    width: 15,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
